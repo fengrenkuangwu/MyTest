ac466cbc242fe2cdbe4a2e6cf5d379015e37250c
fengrenkuangwu <fengrenkuangwu@github.com>
2016-08-21 00:31:20 +0800

    提交缓存测试方法

---
+++ src/com/leaf/memcache/TestMemacaheWithXml.java
@@ -0,0 +1,29 @@
+package com.leaf.memcache;
+
+import java.util.ArrayList;  
+import java.util.List;  
+  
+import com.alisoft.xplatform.asf.cache.ICacheManager;  
+import com.alisoft.xplatform.asf.cache.IMemcachedCache;  
+import com.alisoft.xplatform.asf.cache.memcached.CacheUtil;  
+import com.alisoft.xplatform.asf.cache.memcached.MemcachedCacheManager;  
+  
+public class TestMemacaheWithXml {  
+      
+    @SuppressWarnings("unchecked")  
+    public static void main(String[] args) {  
+        ICacheManager<IMemcachedCache> manager;  
+        manager = CacheUtil.getCacheManager(IMemcachedCache.class,  
+                MemcachedCacheManager.class.getName());  
+        manager.setConfigFile("memcached.xml");  
+        manager.start();  
+        try {  
+            IMemcachedCache cache = manager.getCache("mclient_0");  
+            cache.put("key", "valuesdfsdf");  
+            System.out.println(cache.get("key"));  
+        } finally {  
+            manager.stop();  
+        }  
+    }  
+  
+}  
---
+++ src/com/leaf/memcache/TestMemcache.java
@@ -0,0 +1,56 @@
+package com.leaf.memcache;
+
+import java.util.Date;  
+
+import com.danga.MemCached.MemCachedClient;  
+import com.danga.MemCached.SockIOPool;  
+  
+/**
+ * 1) 简介
+   这是比较通用的Memcached客户端框架。具体原创不详。
+   2) 依赖的jar
+	A. commons-pool-1.5.6.jar
+	B. java_memcached-release_2.6.3.jar
+	C. slf4j-api-1.6.1.jar
+	D. slf4j-simple-1.6.1.jar
+ * @author leaf
+ *
+ */
+
+public class TestMemcache {  
+    public static void main(String[] args) {  
+        MemCachedClient client=new MemCachedClient();  
+        String [] addr ={"127.0.0.1:11211"};  
+        Integer [] weights = {3};  
+        SockIOPool pool = SockIOPool.getInstance();  
+        pool.setServers(addr);  
+        pool.setWeights(weights);  
+        pool.setInitConn(5);  
+        pool.setMinConn(5);  
+        pool.setMaxConn(200);  
+        pool.setMaxIdle(1000*30*30);  
+        pool.setMaintSleep(30);  
+        pool.setNagle(false);  
+        pool.setSocketTO(30);  
+        pool.setSocketConnectTO(0);  
+        pool.initialize();  
+          
+//      String [] s  =pool.getServers();  
+//        client.setCompressEnable(true);  
+//        client.setCompressThreshold(1000*1024);  
+        
+//      将数据放入缓存  
+        client.set("test2","test2");  
+          
+//      将数据放入缓存,并设置失效时间  
+        Date date=new Date(2000000);  
+        client.set("test1","test1", date);  
+          
+//      删除缓存数据  
+//      client.delete("test1");  
+          
+//      获取缓存数据  
+        String str =(String)client.get("test1");  
+        System.out.println(str);  
+    }  
+}  
